ed4c598473e2f63c6aacc4cd40769cf841c8beed
BlastChuck <astolfigio@gmail.com>
2026-05-03 00:21:51 +0200

    mnodifiche finali

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const tasks = [
-  { name: 'Gantt', owner: 'Viola Scappazzoni', status: 'completed', start: '2026-04-08', end: '2026-05-03' },
-  { name: 'Sito web con database', owner: 'Giorgio Astolfi', status: 'completed', start: '2026-04-20', end: '2026-04-27' },
-  { name: 'Sito per presentazione', owner: 'Giorgio Astolfi', status: 'completed', start: '2026-04-20', end: '2026-04-27' },
-  { name: 'Analisi dei requisiti', owner: 'Viola Scappazzoni', status: 'completed', start: '2026-03-25', end: '2026-03-30' },
+  { name: 'Analisi dei requisiti', owner: 'Viola Scappazzoni, Andrea Burlui, Giorgio Astolfi', status: 'completed', start: '2026-03-25', end: '2026-03-30' },
+  { name: 'Individuare le entità, attributi e pk', owner: 'Viola Scappazzoni', status: 'completed', start: '2026-03-29', end: '2026-03-29' },
+  { name: 'Suddivisione dei compiti', owner: 'Giorgio Astolfi, Andrea Burlui, Viola Scappazzoni', status: 'completed', start: '2026-03-30', end: '2026-03-30' },
+  { name: 'Riformulazione della consegna', owner: 'Giorgio Astolfi, Andrea Burlui, Viola Scappazzoni', status: 'completed', start: '2026-03-30', end: '2026-03-30' },
   { name: 'Modello ER', owner: 'Viola Scappazzoni, Giorgio Astolfi', status: 'completed', start: '2026-03-30', end: '2026-04-17' },
+  { name: 'Modello su designer polito', owner: 'Giorgio Astolfi', status: 'completed', start: '2026-03-30', end: '2026-04-15' },
+  { name: 'Modello ristrutturato', owner: 'Viola Scappazzoni', status: 'completed', start: '2026-04-15', end: '2026-04-17' },
   { name: 'Versione 1', owner: 'Team', status: 'completed', start: '2026-03-25', end: '2026-04-01' },
   { name: 'Versione 2', owner: 'Team', status: 'completed', start: '2026-04-01', end: '2026-04-07' },
   { name: 'Versione 3', owner: 'Team', status: 'completed', start: '2026-04-07', end: '2026-04-13' },
-  { name: 'Modello ristrutturato', owner: 'Viola Scappazzoni', status: 'completed', start: '2026-04-15', end: '2026-04-17' },
   { name: 'Modello logico', owner: 'Andrea Burlui', status: 'completed', start: '2026-04-15', end: '2026-04-22' },
+  { name: 'Entità, attributi e pk', owner: 'Andrea Burlui', status: 'completed', start: '2026-04-12', end: '2026-04-12' },
+  { name: 'Vincoli referenziali', owner: 'Andrea Burlui', status: 'completed', start: '2026-04-20', end: '2026-04-20' },
   { name: 'Modello fisico', owner: 'Giorgio Astolfi, Viola Scappazzoni', status: 'completed', start: '2026-04-17', end: '2026-04-20' },
   { name: 'Creazione database vuoto', owner: 'Viola Scappazzoni', status: 'completed', start: '2026-04-17', end: '2026-04-17' },
-  { name: 'Popolamento database', owner: 'Giorgio Astolfi', status: 'completed', start: '2026-04-20', end: '2026-04-20' },
+  { name: 'Popolamento database', owner: 'Giorgio Astolfi, Andrea Burlui, Viola Scappazzoni', status: 'completed', start: '2026-04-20', end: '2026-04-20' },
   { name: 'Test', owner: 'Andrea Burlui', status: 'completed', start: '2026-04-22', end: '2026-04-27' },
-  { name: 'Presetazione', owner: 'Giorgio Astolfi', status: 'completed', start: '2026-04-20', end: '2026-05-03' }
+  { name: 'Presentazione', owner: 'Giorgio Astolfi', status: 'completed', start: '2026-04-20', end: '2026-05-03' },
+  { name: 'Gantt', owner: 'Viola Scappazzoni', status: 'completed', start: '2026-04-08', end: '2026-05-03' },
+  { name: 'Sito web con database', owner: 'Giorgio Astolfi', status: 'completed', start: '2026-04-20', end: '2026-04-27' },
+  { name: 'Sito per presentazione', owner: 'Giorgio Astolfi', status: 'completed', start: '2026-04-20', end: '2026-04-27' }
 ];
 
 const timelineBody = document.getElementById('timeline-body');
